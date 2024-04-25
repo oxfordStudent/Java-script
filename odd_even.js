6cfa354ce1f1dcc445e03c3a0e9ebb4b5a298e6f
Abhay <162295684+Yadavji1003@users.noreply.github.com>
2024-04-25 11:24:13 +0530

    find the odd and even.

---
+++ odd_even.js
@@ -0,0 +1,25 @@
+let num1 = Number( prompt("Enter first Number"));
+let num2 = Number( prompt("Enter Second Number"));
+let num = num1+num2 
+alert(num)
+alert(num%2==0);             
+//  If num returns true it means number is even.
+// If num returns false it means number is odd.
+
+// Find the Sum of two number is even or odd ?
+
+// let num1 = Number( prompt("Enter first Number"));
+// let num2 = Number( prompt("Enter Second Number"));
+// let num = num1+num2 
+// alert(num)
+// if(num%2==0){
+//     alert( "The number is Even.")
+// }
+// else{
+//     alert( "The number is Odd.")
+// }
+
+    
+
+// use of passint And Passfloat .
+ 
